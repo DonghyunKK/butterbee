e360c3961146507cca3928af55ae59226b062bde
donghyun kim <donghyunkim524@gmail.com>
2021-02-12 14:42:54 +0000

    add FAQ and Pricelist routes and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/pricelist', to: 'pages#pricelist', as: 'pricelist'
+  get '/faq', to: 'pages#faq', as: 'faq'
+  get '/cake_order', to: 'pages#cake_order', as: 'cake_order'
+  get '/cupcake_order', to: 'pages#cupcake_order', as: 'cupcake_order'
   resources :cakes
   resources :cupcakes
 end
