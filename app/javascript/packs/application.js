5c788957d962b46f96722907939c688e2b66ce26
donghyun kim <donghyunkim524@gmail.com>
2021-02-20 15:48:49 +0000

    add navbar active class JS and title for gallery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,6 +25,7 @@ require("channels")
 // External imports
 import "bootstrap";
 import "controllers"
+import { navbarActive } from "../components/navbar";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -32,4 +33,5 @@ import "controllers"
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  navbarActive();
 });
