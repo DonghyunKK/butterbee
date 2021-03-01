437b2fd97e5c7880cb0b57d5cf7a05c4e93f54b2
donghyun kim <donghyunkim524@gmail.com>
2021-03-01 16:38:05 +0000

    fixing order form

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,4 @@
-class Order < ApplicationMailer
+class Order < MailForm::Base
   attributes :name, validate: true
   attributes :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attributes :message
