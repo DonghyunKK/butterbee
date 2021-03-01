c839ccf3354ffc5922f30297fa622c86a53b76a9
donghyun kim <donghyunkim524@gmail.com>
2021-03-01 15:41:14 +0000

    Added sendgrid production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,18 +92,18 @@ Rails.application.configure do
   config.force_ssl = true
 
   # Set up ActionMailer 
-  # config.action_mailer.default_url_options = { host: 'https://www.butterbeebakery.co.uk'}
-  # config.action_mailer.delivery_method = :stmp
+  config.action_mailer.default_url_options = { host: 'https://www.butterbeebakery.co.uk'}
+  config.action_mailer.delivery_method = :stmp
 
-  # ActionMailer::Base.smtp_settings = {
-  #   :user_name => ENV['SENDGRID_USERNAME'],
-  #   :password => ENV['SENDGRID_PASSWORD'],
-  #   :domain => 'example.com',
-  #   :address => 'smtp.sendgrid.net',
-  #   :port => 587,
-  #   :authentication => :plain,
-  #   :enable_starttls_auto => true
-  # }
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'butterbeebakery.co.uk',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
