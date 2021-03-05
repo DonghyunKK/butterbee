73d7244667f1288f499c7cdbf19c5c62fa39d03b
donghyun kim <donghyunkim524@gmail.com>
2021-03-05 11:54:14 +0000

    fixing multiple attachments / change attribute

--- app/models/cake_order.rb
+++ app/models/cake_order.rb
@@ -9,7 +9,7 @@ class CakeOrder < MailForm::Base
   attribute :address
   attribute :postcode
   attribute :due_date, validate: true
-  attribute :photos, attachment: true, has_many_attachment: true
+  attribute :photos, attachment: true
   attribute :nickname, captcha: true
 
   def headers
