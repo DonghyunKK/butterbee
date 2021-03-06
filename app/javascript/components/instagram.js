ee904d4275f707d7c5d4f868f7009f0d9ea8eebe
donghyun kim <donghyunkim524@gmail.com>
2021-03-06 22:42:00 +0000

    add instgram feed but need more work

--- app/javascript/components/instagram.js
+++ app/javascript/components/instagram.js
@@ -7,54 +7,59 @@
 
 const fetchFeed = () => {
 
-  // let api_endpoint = "https://v1.nocodeapi.com/donghyunkk/instagram/pFWHZMNRoxkKrXev";
-  
-  // $.get(api_endpoint, function( json ) {
-  // let instabanner = $('#instafeed');
-  // let outputHtml = "";
-  // outputHtml += '<ul class="myfeed">';
-  
-  // let limit = 12;
-  // let myfeed = json.data;
-  
-  // for (let i = 0; i < limit; i++) {
-  //   outputHtml += '<li>';
-  //   outputHtml += '<a href="{{link}}"><img title="{{caption}}" src="{{image}}" /></a>'
-  //   outputHtml += '<li>';
+  // const buildFeedHtml = () => {
+  //   feed = `<ul class="feed-box">
+  //             <li class="feed-image" style="background-image: url(${});">
+  //             <a href="${permalink}"><img title="{{caption}}" src="{{image}}" /></a>
+  //             </li>
+  //           </ul>`
   // }
+
+  let url = "https://v1.nocodeapi.com/donghyunkk/instagram/pFWHZMNRoxkKrXev";
+  // fetch(url)
+  //   .then(response => response.json())
+  //   .then((data) => {
+      
+  //   })
   
-  // outputHtml += '<ul>';
-  // instabanner.html( outputHtml );
+
+  // var settings = {
+  //   "url": "https://v1.nocodeapi.com/donghyunkk/instagram/pFWHZMNRoxkKrXev?",
+  //   "method": "get",
+  //   "timeout": 0,
+  //   "headers": {
+  //       "Content-Type": "application/json"
+  //     },
+  // };
+
+  //  $.ajax(settings).done(function (response) {
+  //    console.log(response.data)
+  //  });
   
-  // })
-  var settings = {
-    "url": "https://v1.nocodeapi.com/donghyunkk/instagram/pFWHZMNRoxkKrXev?",
-    "method": "get",
-    "timeout": 0,
-    "headers": {
-        "Content-Type": "application/json"
-      },
-  };
-  
-  $.ajax(settings).done(function (response) {
+  $.get(url, function (json) {
     let instabanner = $('#instafeed');
     let outputHtml = "";
     outputHtml += '<ul class="myfeed">';
     
     let limit = 12;
-    let myfeed = response.data;
+    let myfeed = json.data;
+    console.log(myfeed)
     
     for (let i = 0; i < limit; i++) {
       outputHtml += '<li>';
-      outputHtml += '<a href="{{link}}"><img title="{{caption}}" src="{{image}}" /></a>'
-      outputHtml += '<li>';
+      if (myfeed[i].media_type == 'VIDEO') {
+        outputHtml += '<i class="fas fa-video"><a target="_blank" herf="' + myfeed[i].permalink+'" style="background-image: url('+myfeed[i].media_url+')"></a>';
+      } else {
+        outputHtml += '<a target="_blank" herf="'+myfeed[i].permalink+'" style="background-image: url('+myfeed[i].media_url+')"></a>';
+      }
+      outputHtml += '</li>';
     }
     
-    outputHtml += '<ul>';
+    outputHtml += '</ul>';
     instabanner.html( outputHtml );
   
   });
-}
+};
 
 // const insta = () => {
   //   var feed = new Instafeed({
