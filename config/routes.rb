88c1c550c0de6eb67230d9657587685e102e64da
donghyun kim <donghyunkim524@gmail.com>
2021-03-09 12:07:17 +0000

    edit text and style

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/pricelist', to: 'pages#pricelist', as: 'pricelist'
   get '/faq', to: 'pages#faq', as: 'faq'
   get '/terms', to: 'pages#terms', as: 'terms'
-  get '/private', to: 'pages#private', as: 'private'
+  get '/privacy', to: 'pages#privacy', as: 'privacy'
   get '/confirm', to: 'pages#confirm', as: 'confirm'
   # get '/cake_order', to: 'pages#cake_order', as: 'cake_order'
   # get '/cupcake_order', to: 'pages#cupcake_order', as: 'cupcake_order'
