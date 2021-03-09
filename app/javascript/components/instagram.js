ca0d80d62ed2a7aaff65972e40221913ee9caf09
donghyun kim <donghyunkim524@gmail.com>
2021-03-09 21:51:55 +0000

    fix fetch instagram to only for home page

--- app/javascript/components/instagram.js
+++ app/javascript/components/instagram.js
@@ -1,6 +1,7 @@
 const fetchFeed = () => {
   let url = "https://v1.nocodeapi.com/donghyunkk/instagram/pFWHZMNRoxkKrXev";
-  fetch(url)
+  if (window.location.pathname == "/") {
+    fetch(url)
     .then(response => response.json())
     .then((data) => {
       const limit = 9
@@ -8,10 +9,11 @@ const fetchFeed = () => {
       for (let i = 0; i < limit; i++) {
         document.querySelector(".feed-box").insertAdjacentHTML('beforeEnd', 
         // ` <li class="feed-image" style="background-image: url(${feed[i].media_url});">
-          `<a class="feed-link" href="${feed[i].permalink}" target="_blank"><img class="feed-image" src="${feed[i].media_url}"></a>`);
-          // </li>`);
+        `<a class="feed-link" href="${feed[i].permalink}" target="_blank"><img class="feed-image" src="${feed[i].media_url}"></a>`);
+        // </li>`);
       }
     })
+  }
 }
 
 export { fetchFeed };
