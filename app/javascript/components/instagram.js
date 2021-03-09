92796708e330f14e2f5ec8bbdfd2de31dd6329c2
donghyun kim <donghyunkim524@gmail.com>
2021-03-09 21:21:21 +0000

    delete testing code from js

--- app/javascript/components/instagram.js
+++ app/javascript/components/instagram.js
@@ -3,8 +3,6 @@ const fetchFeed = () => {
   fetch(url)
     .then(response => response.json())
     .then((data) => {
-      console.log(data)
-      console.log(data.data)
       const limit = 9
       const feed = data.data
       for (let i = 0; i < limit; i++) {
@@ -15,70 +13,5 @@ const fetchFeed = () => {
       }
     })
 }
-
-// const fetchFeed = () => {
-
-//   // const buildFeedHtml = () => {
-//   //   feed = `<ul class="feed-box">
-//   //             <li class="feed-image" style="background-image: url(${});">
-//   //             <a href="${permalink}"><img title="{{caption}}" src="{{image}}" /></a>
-//   //             </li>
-//   //           </ul>`
-//   // }
-
-//   let url = "https://v1.nocodeapi.com/donghyunkk/instagram/pFWHZMNRoxkKrXev";
-//   // fetch(url)
-//   //   .then(response => response.json())
-//   //   .then((data) => {
-      
-//   //   })
-  
-
-//   // var settings = {
-//   //   "url": "https://v1.nocodeapi.com/donghyunkk/instagram/pFWHZMNRoxkKrXev?",
-//   //   "method": "get",
-//   //   "timeout": 0,
-//   //   "headers": {
-//   //       "Content-Type": "application/json"
-//   //     },
-//   // };
-
-//   //  $.ajax(settings).done(function (response) {
-//   //    console.log(response.data)
-//   //  });
-  
-//   $.get(url, function (json) {
-//     let instabanner = $('#instafeed');
-//     let outputHtml = "";
-//     outputHtml += '<ul class="myfeed">';
-    
-//     let limit = 12;
-//     let myfeed = json.data;
-//     console.log(myfeed)
-    
-//     for (let i = 0; i < limit; i++) {
-//       outputHtml += '<li>';
-//       if (myfeed[i].media_type == 'VIDEO') {
-//         outputHtml += '<i class="fas fa-video"><a target="_blank" herf="' + myfeed[i].permalink+'" style="background-image: url('+myfeed[i].media_url+')"></a>';
-//       } else {
-//         outputHtml += '<a target="_blank" herf="'+myfeed[i].permalink+'" style="background-image: url('+myfeed[i].media_url+')"></a>';
-//       }
-//       outputHtml += '</li>';
-//     }
-    
-//     outputHtml += '</ul>';
-//     instabanner.html( outputHtml );
-  
-//   });
-// };
-
-// // const insta = () => {
-//   //   var feed = new Instafeed({
-//     //     target: 'instafeed',
-//     //     accessToken: 'https://v1.nocodeapi.com/donghyunkk/instagram/pFWHZMNRoxkKrXev',
-// //     template: '<a href="{{link}}"><img title="{{caption}}" src="{{image}}" /></a>'
-// //   });
-// //   feed.run();
-// // }
 
 export { fetchFeed };
