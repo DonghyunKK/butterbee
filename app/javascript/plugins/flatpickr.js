49582e1061469f313ec2beb1c7221ccdd55574e1
donghyun kim <donghyunkim524@gmail.com>
2021-05-28 10:27:32 +0100

    edit disable order date

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -20,7 +20,16 @@ const initFlatpickr = () => {
         {
           from: "2021-06-21",
           to: "2021-06-27"
-        }
+        },
+        {
+          from: "2021-08-21",
+          to: "2021-08-22"
+        },
+        {
+          from: "2021-10-11",
+          to: "2021-11-07"
+        },
+        "2021-06-12", "2021-07-17"
     ],
     "locale": {
         "firstDayOfWeek": 1 // start week on Monday
